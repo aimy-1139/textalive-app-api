b428910216be5fd1333fdc32952a8802ba4847bb
aimy-1139 <275624305+aimy-1139@users.noreply.github.com>
2026-04-14 10:25:19 +0900

    Create index.K.js

---
+++ dist/index.K.js
@@ -0,0 +1,269 @@
+**
+ * TextAlive App API lyric sheet example
+ * https://github.com/TextAliveJp/textalive-app-lyric-sheet
+ *
+ * インタラクティブな歌詞カードを実装した TextAlive App API のサンプルコードです。
+ * 発声にあわせて歌詞が表示され、歌詞をクリックするとそのタイミングに再生がシークします。
+ * また、このアプリが TextAlive ホストと接続されていなければ再生コントロールを表示します。
+ */
+const { Player } = TextAliveApp;
+
+// TextAlive Player を初期化
+const player = new Player({
+  // トークンは https://developer.textalive.jp/profile で取得したものを使う
+  app: {
+    token: "70Pnpz2O7ut9TULy",
+    parameters: [
+      {
+        title: "Gradation start color",
+        name: "gradationStartColor",
+        className: "Color",
+        initialValue: "#bfaefc",
+      },
+      {
+        title: "Gradation middle color",
+        name: "gradationMiddleColor",
+        className: "Color",
+        initialValue: "#fea3db"
+      },
+      {
+        title: "Gradation end color",
+        name: "gradationEndColor",
+        className: "Color",
+        initialValue: "#9ae1dd",
+      },
+    ],
+  },
+
+  mediaElement: document.querySelector("#media"),
+  mediaBannerPosition: "bottom right",
+
+  // オプション一覧
+  // https://developer.textalive.jp/packages/textalive-app-api/interfaces/playeroptions.html
+});
+
+const overlay = document.querySelector("#overlay");
+const bar = document.querySelector("#bar");
+const textContainer = document.querySelector("#text");
+const seekbar = document.querySelector("#seekbar");
+const paintedSeekbar = seekbar.querySelector("div");
+let lastTime = -1;
+
+player.addListener({
+  /* APIの準備ができたら呼ばれる */
+  onAppReady(app) {
+    if (app.managed) {
+      document.querySelector("#control").className = "disabled";
+    }
+    if (!app.songUrl) {
+      document.querySelector("#media").className = "disabled";
+
+      // ストリートライト / 加賀(ネギシャワーP)
+      player.createFromSongUrl("https://piapro.jp/t/ULcJ/20250205120202", {
+        video: {
+          // 音楽地図訂正履歴
+          beatId: 4694275,
+          chordId: 2830730,
+          repetitiveSegmentId: 2946478,
+      
+          // 歌詞URL: https://piapro.jp/t/DPXV
+          // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FULcJ%2F20250205120202
+          lyricId: 67810,
+          lyricDiffId: 20654
+        },
+      });
+    }
+  },
+
+  /* パラメタが更新されたら呼ばれる */
+  onAppParameterUpdate: () => {
+    const params = player.app.options.parameters;
+    const sc = player.app.parameters.gradationStartColor,
+      scString = sc ? `rgb(${sc.r}, ${sc.g}, ${sc.b})` : params[0].initialValue;
+    const mc = player.app.parameters.gradationMiddleColor,
+      mcString = mc ? `rgb(${mc.r}, ${mc.g}, ${mc.b})` : params[0].initialValue;
+    const ec = player.app.parameters.gradationEndColor,
+      ecString = ec ? `rgb(${ec.r}, ${ec.g}, ${ec.b})` : params[1].initialValue;
+    document.body.style.backgroundColor = ecString;
+    document.body.style.backgroundImage = `linear-gradient(0deg, ${ecString} 0%, ${mcString} 50%, ${scString} 100%)`;
+  },
+
+  /* 楽曲が変わったら呼ばれる */
+  onAppMediaChange() {
+    // 画面表示をリセット
+    overlay.className = "";
+    bar.className = "";
+    resetChars();
+  },
+
+  /* 楽曲情報が取れたら呼ばれる */
+  onVideoReady(video) {
+    // 楽曲情報を表示
+    document.querySelector("#artist span").textContent =
+      player.data.song.artist.name;
+    document.querySelector("#song span").textContent = player.data.song.name;
+
+    // 最後に取得した再生時刻の情報をリセット
+    lastTime = -1;
+  },
+
+  /* 再生コントロールができるようになったら呼ばれる */
+  onTimerReady() {
+    overlay.className = "disabled";
+    document.querySelector("#control > a#play").className = "";
+    document.querySelector("#control > a#stop").className = "";
+  },
+
+  /* 再生位置の情報が更新されたら呼ばれる */
+  onTimeUpdate(position) {
+    // シークバーの表示を更新
+    paintedSeekbar.style.width = `${
+      parseInt((position * 1000) / player.video.duration) / 10
+    }%`;
+
+    // 新しいビートを検出
+    const beats = player.findBeatChange(lastTime, position);
+    if (
+      lastTime >= 0 &&
+      // ↑初期化された直後はビート検出しない
+      beats.entered.length > 0
+      // ↑二拍ごとにしたければ
+      //   && beats.entered.find((b) => b.position % 2 === 1)
+      // のような条件を足してチェックすればよい
+    ) {
+      // ビート同期のアニメーションを発火させる
+      requestAnimationFrame(() => {
+        bar.className = "active";
+        requestAnimationFrame(() => {
+          bar.className = "active beat";
+        });
+      });
+    }
+
+    // 歌詞情報がなければこれで処理を終わる
+    if (!player.video.firstChar) {
+      return;
+    }
+
+    // 巻き戻っていたら歌詞表示をリセットする
+    if (lastTime > position + 1000) {
+      resetChars();
+    }
+
+    // 500ms先に発声される文字を検出
+    // 初回は開始時からの差分区間、それ以降は前回実行時からの差分区間を検出
+    const chars = player.video.findCharChange(lastTime < 0 ? lastTime : lastTime + 500, position + 500);
+    for (const c of chars.entered) {
+      // 新しい文字が発声されようとしている
+      newChar(c);
+    }
+
+    // 次回呼ばれるときのために再生時刻を保存しておく
+    lastTime = position;
+  },
+
+  /* 楽曲の再生が始まったら呼ばれる */
+  onPlay() {
+    const a = document.querySelector("#control > a#play");
+    while (a.firstChild) a.removeChild(a.firstChild);
+    a.appendChild(document.createTextNode("\uf28b"));
+  },
+
+  /* 楽曲の再生が止まったら呼ばれる */
+  onPause() {
+    const a = document.querySelector("#control > a#play");
+    while (a.firstChild) a.removeChild(a.firstChild);
+    a.appendChild(document.createTextNode("\uf144"));
+  },
+});
+
+/* 再生・一時停止ボタン */
+document.querySelector("#control > a#play").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (player) {
+    if (player.isPlaying) {
+      player.requestPause();
+    } else {
+      player.requestPlay();
+    }
+  }
+  return false;
+});
+
+/* 停止ボタン */
+document.querySelector("#control > a#stop").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (player) {
+    player.requestStop();
+
+    // 再生を停止したら画面表示をリセットする
+    bar.className = "";
+    resetChars();
+  }
+  return false;
+});
+
+/* シークバー */
+seekbar.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (player) {
+    player.requestMediaSeek(
+      (player.video.duration * e.offsetX) / seekbar.clientWidth
+    );
+  }
+  return false;
+});
+
+/**
+ * 新しい文字の発声時に呼ばれる
+ * Called when a new character is being vocalized
+ */
+function newChar(current) {
+  // 品詞 (part-of-speech)
+  // https://developer.textalive.jp/packages/textalive-app-api/interfaces/iword.html#pos
+  const classes = [];
+  if (
+    current.parent.pos === "N" ||
+    current.parent.pos === "PN" ||
+    current.parent.pos === "X"
+  ) {
+    classes.push("noun");
+  }
+
+  // フレーズの最後の文字か否か
+  if (current.parent.parent.lastChar === current) {
+    classes.push("lastChar");
+  }
+
+  // 英単語の最初か最後の文字か否か
+  if (current.parent.language === "en") {
+    if (current.parent.lastChar === current) {
+      classes.push("lastCharInEnglishWord");
+    } else if (current.parent.firstChar === current) {
+      classes.push("firstCharInEnglishWord");
+    }
+  }
+
+  // noun, lastChar クラスを必要に応じて追加
+  const div = document.createElement("div");
+  div.appendChild(document.createTextNode(current.text));
+
+  // 文字を画面上に追加
+  const container = document.createElement("div");
+  container.className = classes.join(" ");
+  container.appendChild(div);
+  container.addEventListener("click", () => {
+    player.requestMediaSeek(current.startTime);
+  });
+  textContainer.appendChild(container);
+}
+
+/**
+ * 歌詞表示をリセットする
+ * Reset lyrics view
+ */
+function resetChars() {
+  lastTime = -1;
+  while (textContainer.firstChild)
+    textContainer.removeChild(textContainer.firstChild);
+}
